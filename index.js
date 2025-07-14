5ed827123c1856f7d93126c33f7a1d5107862252
JamalHibou <jamal@hibou.nl>
2025-07-14 14:56:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { chromium } from 'playwright';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.get('/', async (req, res) => {
   const browser = await chromium.launch({ headless: true });
